759c2de523938147c44074651c060094c6db2b7b
Unai Goikoetxeta <ugoikoetxeta@wavebroadband.com>
2020-08-12 19:26:33 -0700

    First commit

---
+++ bga-original-carcassonne-tiles.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Original Carcassone Tiles
+// @namespace    https://github.com/bockbilbo/bga-original-carcassonne-tiles
+// @version      1.0
+// @description  Displays the original carcassone tiles in boardgamearena.com when selecting the "Latest" "Artwork edition" from the playing menu.
+// @author       bockbilbo
+// @match        https://boardgamearena.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle (".second_edition { background-image: url('https://raw.githubusercontent.com/bockbilbo/bga-old-carcassonne-tiles/master/original-tiles.jpg') !important; }");
+})();
